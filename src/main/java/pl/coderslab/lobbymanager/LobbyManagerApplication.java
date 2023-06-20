b83dd8b4609db842cc7fa4c0d18f383c5bd8fdbd
Karol <karol.zimolag@wp.pl>
2023-06-20 16:05:10 +0200

    First Commit

---
+++ src/main/java/pl/coderslab/lobbymanager/LobbyManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.lobbymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/lobbymanager/LobbyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.lobbymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
